88f432cac89cc763edd49c68238c2b021dd75c39
Szkrishna <krisnas9792@gmail.com>
2025-01-09 18:07:57 +0530

    created books.py file to start project flow and created books model using pydantic basemodel and field

---
+++ books.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str = Field(min_length=1)
+    author: str = Field(min_length=1, max_length=100)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=101)
+    
+
+
+@app.get("/{name}")
+def read_root(name: str):
+    return { "Hello": name }
